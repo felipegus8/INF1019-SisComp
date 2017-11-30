10df72f540047a31a32d2d30248b6c1b96dcbb30
Matheus Rodrigues de Oliveira Leal <mrol@br.ibm.com>
2017-11-30 10:37:50 -0200

    func pegaIndicePagina

--- Trabalho_2/main.c
+++ Trabalho_2/main.c
@@ -3,8 +3,24 @@
 #include <string.h>
 #include <unistd.h>
 
+unsigned pegaIndicePagina(unsigned addr){
+	unsigned page = addr >> 16;
+	return page;
+}
+
 int main(void){
   int P1,P2,P3,P4;
+
+	int *memoria_fisica = (int *)malloc(sizeof(int) * 256);
+
+	if(!memoria_fisica) {
+		printf("Erro na alocacao da memoria fisica.\n");
+		exit(1);
+	}
+
+	for(i = 0; i < 256; i++) {
+		memoria_fisica[i] = -1;
+	}
 
   P1 = fork();
   if(P1 != 0){
@@ -21,37 +37,45 @@ int main(void){
 
         }else{
           //Processo Filho 4 (simulador.log)
-          unsigned addr4, rw4;
+          unsigned addr4;
+					char rw4;
           File *arq4 = fopen("simulador.log", "r");
 
           while( fscanf(arq4, "%x %c", &addr4, &rw4) == 2 ) {
+						int indicePagina = pegaIndicePagina(addr4);
 
           }
         }
       }else{
         //Processo Filho 3 (compressor.log)
-        unsigned addr3, rw3;
+        unsigned addr3;
+				char rw3;
         File *arq3 = fopen("compressor.log", "r");
 
         while( fscanf(arq3, "%x %c", &addr3, &rw3) == 2 ) {
+					int indicePagina = pegaIndicePagina(addr3);
 
         }
       }
     }else{
       //Processo Filho 2 (matrix.log)
-      unsigned addr2, rw2;
+      unsigned addr2;
+			char rw2;
       File *arq2 = fopen("matrix.log", "r");
 
 	    while( fscanf(arq2, "%x %c", &addr2, &rw2) == 2 ) {
+				int indicePagina = pegaIndicePagina(addr2);
 
       }
     }
   }else{
     //Processo Filho 1 (compilador.log)
-    unsigned addr1, rw1;
+    unsigned addr1;
+		char rw1;
     File *arq1 = fopen("compilador.log", "r");
 
     while( fscanf(arq1, "%x %c", &addr1, &rw1) == 2 ) {
+			int indicePagina = pegaIndicePagina(addr1);
 
     }
   }
