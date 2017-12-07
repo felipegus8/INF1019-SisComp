9996708484ac56e02eed2ff9673dde5a68dd6ea5
Matheus Rodrigues de Oliveira Leal <mrol@br.ibm.com>
2017-12-07 16:15:19 -0200

    updates

--- Trabalho_2/main.c
+++ Trabalho_2/main.c
@@ -2,13 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#define MAXTABELA 256 //NAO SEI O TAMANHO DA TABELA DE PAGINAS
+#define MAXTABELA 65536//NAO SEI O TAMANHO DA TABELA DE PAGINAS
 
 typedef struct tabelaPagina {
 	char r_ou_w;
-	int modificado;
-	int ta_no_frame;
-	int numero_frame;
+	int id_processo;
 	int acesso; //contador de acessos
 } TabelaPagina;
 
@@ -23,13 +21,18 @@ int busca_menos_acessado(TabelaPagina *t){
 	return menor;
 }
 
-TabelaPagina *criaVetorTabelaPaginas(int paginaTam) {
+TabelaPagina *criaVetorTabelaPaginas(int paginaTam, int id) {
 	TabelaPagina *vetorTabelaPaginas;
 
-	vetorTabelaPaginas = (TabelaPagina *)malloc(sizeof(TabelaPagina) *MAXTABELA ); //NAO SEI O TAMANHO DA TABELA DE PAGINAS
+	segmento = shmget (id, paginaTam*sizeof(int), IPC_CREAT | S_IRUSR | S_IWUSR);
+	vetorTabelaPaginas = (TabelaPagina *) shmat (segmento, 0, 0);
 	if(!vetorTabelaPaginas) {
 		printf("Faltou memoria\n");
 		exit(1);
+	}
+
+	for(i=0;i < MAXTABELA; i++){
+		t[i].acesso = 0;
 	}
 
 	return vetorTabelaPaginas;
@@ -43,14 +46,7 @@ unsigned pegaIndicePagina(unsigned addr){
 int main(void){
   int P1,P2,P3,P4;
 	int segmento;
-	int *memoria_fisica;
-
-	segmento = shmget (8180, 256*sizeof(int), IPC_CREAT | S_IRUSR | S_IWUSR);
-	memoria_fisica = (int*) shmat (segmento, 0, 0);
-	if(!memoria_fisica) {
-		printf("Erro na alocacao da memoria fisica.\n");
-		exit(1);
-	}
+	int memoria_fisica[256];
 
 	for(i = 0; i < 256; i++) {
 		memoria_fisica[i] = -1;
@@ -74,7 +70,7 @@ int main(void){
           unsigned addr4;
 					char rw4;
           File *arq4 = fopen("simulador.log", "r");
-					TabelaPagina *vetorTabelaPaginas4 = criaVetorTabelaPaginas();
+					TabelaPagina *vetorTabelaPaginas4 = criaVetorTabelaPaginas(MAXTABELA,8081);
 
           while( fscanf(arq4, "%x %c", &addr4, &rw4) == 2 ) {
 						int indicePagina = pegaIndicePagina(addr4);
@@ -86,7 +82,7 @@ int main(void){
         unsigned addr3;
 				char rw3;
         File *arq3 = fopen("compressor.log", "r");
-				TabelaPagina *vetorTabelaPaginas3 = criaVetorTabelaPaginas();
+				TabelaPagina *vetorTabelaPaginas3 = criaVetorTabelaPaginas(MAXTABELA,5081);
 
         while( fscanf(arq3, "%x %c", &addr3, &rw3) == 2 ) {
 					int indicePagina = pegaIndicePagina(addr3);
@@ -94,11 +90,11 @@ int main(void){
         }
       }
     }else{
-      //Processo Filho 2 (matrix.log)
+      //Processo Filho 2 (matriz.log)
       unsigned addr2;
 			char rw2;
-      File *arq2 = fopen("matrix.log", "r");
-			TabelaPagina *vetorTabelaPaginas2 = criaVetorTabelaPaginas();
+      File *arq2 = fopen("matriz.log", "r");
+			TabelaPagina *vetorTabelaPaginas2 = criaVetorTabelaPaginas(MAXTABELA,7861);
 
 	    while( fscanf(arq2, "%x %c", &addr2, &rw2) == 2 ) {
 				int indicePagina = pegaIndicePagina(addr2);
@@ -110,7 +106,7 @@ int main(void){
     unsigned addr1;
 		char rw1;
     File *arq1 = fopen("compilador.log", "r");
-		TabelaPagina *vetorTabelaPaginas1 = criaVetorTabelaPaginas();
+		TabelaPagina *vetorTabelaPaginas1 = criaVetorTabelaPaginas(MAXTABELA,1234);
 
     while( fscanf(arq1, "%x %c", &addr1, &rw1) == 2 ) {
 			int indicePagina = pegaIndicePagina(addr1);
